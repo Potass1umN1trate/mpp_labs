3883cae1a22fbbb1976aee3dea63d4fc74b2809d
Daniil Kozyr <dkozyr@mtb.minsk.by>
2025-10-15 17:53:06 +0300

    Something 2

--- todo-next/app/(root)/page.tsx
+++ todo-next/app/(root)/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+//export const revalidate = 1;
+
+export default async function Page() {
+  const data = await fetch("https://api.vercel.app/blog", { next: { revalidate: 1 }});
+  const posts = await data.json();
+
   return (
-    <h1 className="text-3xl">Welcome to Next.js!</h1>
+    <main>
+      <h1>Blog Posts</h1>
+      <ul>
+        {posts.map((post : {id: number, title: string}) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </main>
   )
 }
---
+++ todo-next/app/api/db.ts
@@ -0,0 +1,9 @@
+// app/api/db.ts
+
+const books = [
+    { id: 1, name: "Atomic Habits" },
+    { id: 2, name: "Deep work" },
+    { id: 3, name: "The 7 habits of highly effective people" }    
+];
+
+export default books;
