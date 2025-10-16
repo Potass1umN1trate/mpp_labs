c0bfb0560d0df32b3dc3e057b71834b5ae47b66f
Daniil Kozyr <dkozyr@mtb.minsk.by>
2025-10-16 21:18:13 +0300

    Clean for new project

--- todo-next/app/(root)/about/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-    throw new Error("LOL")
-
-    return (
-        <div>About</div>
-    )
-}
-
-export default Page
--- todo-next/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Loading = () => {
-    return (
-        <div>__________</div>
-    )
-}
-
-export default Loading
